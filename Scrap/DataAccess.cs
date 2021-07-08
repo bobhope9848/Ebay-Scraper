30a027e070dce02a6ca7e60b79e2a21481adb99a
bobhope <william.reindl01@gmail.com>
2021-07-08 15:07:43 -0400

    Add project files.

---
+++ Scrap/DataAccess.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Data.SQLite;
+
+namespace Scrap
+{
+    class DataAccess
+    {
+        private static void CheckDB(string Name)
+        {
+            if (!File.Exists($"{Name}.db")) 
+                {
+                    SQLiteConnection.CreateFile($"{Name}.db");
+                    Console.WriteLine($"Missing {Name}.db");
+                    Console.WriteLine("Creating.....");
+                    string cs = $@"URI=file:{Name}.db";
+                    using var con = new SQLiteConnection(cs);
+                    con.Open();
+                    switch (Name) 
+                    {
+                       case "Enemy":
+                           SQLiteCommand Enemy = new SQLiteCommand("CREATE TABLE Enemies (Name TEXT, Health INT, Armor INT, Attack INT, Speed INT, IsDead INT, Level INT )", con);
+                            Enemy.ExecuteNonQuery();
+                           break;
+                       case "Hero":
+                           SQLiteCommand Hero = new SQLiteCommand("CREATE TABLE Heroes (Name TEXT, Health INT, Armor INT, Attack INT, Speed INT, IsDead INT, Level INT, CurrentXP INT )", con);
+                            Hero.ExecuteNonQuery();
+                           break;
+                       case "Item":
+                            //TODO: Broken ID system when creating
+                           SQLiteCommand Item = new SQLiteCommand("CREATE TABLE Items (ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Speed INT, Armor INT, Health INT, Attack INT, Durability INT, IsEquipped INT, Type INT )", con);
+                            Item.ExecuteNonQuery();
+                            break;
+                       default:
+                            throw new System.ArgumentException($"Invalid name of database {Name}");
+                    }
+                    con.Close();
+                    
+                }
+
+        }
+    }
+}
---
+++ Scrap/Program.cs
@@ -0,0 +1,36 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
+using System.Linq;
+
+namespace Scrap
+{
+    class Program
+    {
+        string CurrentLink;
+
+        static void Main(string[] args)
+        {
+
+            Grab("https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=ps3&_sacat=0&LH_TitleDesc=0&_odkw=pa3&_osacat=0");
+        }
+        static void Grab(string Link)
+        {
+            HttpClient httpClient = new HttpClient();
+            string html = httpClient.GetStringAsync(Link).Result;
+
+            HtmlDocument htmlDocument = new HtmlDocument();
+            
+            htmlDocument.LoadHtml(html);
+
+            var ProductList = htmlDocument.DocumentNode.Descendants().Where(node => node.GetAttributeValue("class", "").Equals("s-item__title")).ToList();
+            foreach (var item in ProductList)
+            {
+                
+                Console.WriteLine(item.InnerText.Replace("New Listing", ""));
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
