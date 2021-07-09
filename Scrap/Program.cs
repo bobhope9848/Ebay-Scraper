f100068148c84511be738208208f0712062cd32c
William Reindl <william.reindl01@gmail.com>
2021-07-09 13:14:11 -0400

    Made Listed struct to store all collected together in list
    Grab method:
    Grabs html document from webpage and then searches for unordered list that contains all listings. After that it breaks it out into 3 parts (Title, Price and URL).
    
    Then recursively goes through List created of all the data listings found and for the moment just outputs to console.

--- Scrap/Program.cs
+++ Scrap/Program.cs
@@ -2,13 +2,28 @@
 using System;
 using System.Net.Http;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace Scrap
 {
     class Program
     {
-        string CurrentLink;
 
+        struct Listed
+        {
+            public String Title { get; set; }
+            public String Url { get; set; }
+            public double Price { get; set; }
+            public Listed(string title, string url, double price)
+            {
+                Title = title;
+                Url = url;
+                Price = price;
+            }
+
+        }
+        static List<Listed> listed = new List<Listed>();
         static void Main(string[] args)
         {
 
@@ -16,18 +31,24 @@ namespace Scrap
         }
         static void Grab(string Link)
         {
+            listed.Add(new Listed("thing", "thing", 48.5));
             HttpClient httpClient = new HttpClient();
             string html = httpClient.GetStringAsync(Link).Result;
-
             HtmlDocument htmlDocument = new HtmlDocument();
             
             htmlDocument.LoadHtml(html);
-
-            var ProductList = htmlDocument.DocumentNode.Descendants().Where(node => node.GetAttributeValue("class", "").Equals("s-item__title")).ToList();
-            foreach (var item in ProductList)
+            var ProductList = htmlDocument.DocumentNode.Descendants().Where(node => node.GetAttributeValue("class", "").Equals("srp-results srp-list clearfix")).First().Descendants().Where(node => node.GetAttributeValue("class", "").Contains("s-item--watch-at-corner")).ToList();
+            var ProductTitle = htmlDocument.DocumentNode.Descendants().Where(node => node.GetAttributeValue("class", "").Equals("s-item__title")).ToList();
+            var ProductPrice = htmlDocument.DocumentNode.Descendants().Where(node => node.GetAttributeValue("class", "").Equals("s-item__price")).ToList();
+            var ProductUrl = htmlDocument.DocumentNode.Descendants().Where(node => node.GetAttributeValue("class", "").Contains("s-item__link")).ToList();
+            for (int i = 0; i < ProductList.Count; i++)
             {
-                
-                Console.WriteLine(item.InnerText.Replace("New Listing", ""));
+                Console.WriteLine(ProductList[i].GetAttributeValue("class", ""));
+                /*
+                Console.WriteLine(Regex.Replace(ProductTitle[i].InnerText.Replace("New Listing", ""), @"[^\u0000-\u007F]+", string.Empty).TrimStart());
+                Console.WriteLine(ProductPrice[i].InnerText);
+                Console.WriteLine(ProductUrl[i].GetAttributeValue("href", ""));
+                */
             }
             Console.ReadLine();
 
